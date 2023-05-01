bc96352ab3f3696fd4adf2b169bf32746debcf6d
Ravinderparmar <132219661+Ravinderparmar@users.noreply.github.com>
2023-05-01 14:30:58 +0530

    Create pattern-1.c

---
+++ pattern/pattern-1.c
@@ -0,0 +1,22 @@
+*
+**
+***
+****
+
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+ main() {
+int i,j;
+for(i=1;i<=4;i++)
+{
+    for(j=1;j<=i;j++)
+{
+    printf("*");
+}
+printf("\n");
+}
+return 0;
+}
