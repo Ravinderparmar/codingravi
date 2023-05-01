af6cc81743ec38f80e0a835c59e8b8ebe9fd25ff
Ravinderparmar <132219661+Ravinderparmar@users.noreply.github.com>
2023-05-01 23:56:19 +0530

    Create pattern3.c

---
+++ pattern/pattern3.c
@@ -0,0 +1,24 @@
+*
+**
+***
+****
+*****
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int r=1,c,n;
+    printf("input range upto you want the pattern");
+    scanf("%d",&n);
+    while (r<=n)
+    {c=1;
+        while(c<=r)
+        {
+            printf("*");
+            c++;
+        }
+        printf("\n");
+        r++;
+    }
+}
